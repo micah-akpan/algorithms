77ed448432e86b146780bc459e0db80999e3f952
Micah Akpan <mickey4sure2k7@gmail.com>
2017-07-03 15:58:04 +0000

    added some comments in main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 function getPrimes(n) {
+	// takes as input, n an int, and returns a list of primes numbers
+	// starting from 0 to n
 	if (n < 0 || typeof n == 'string' || n % 1 !== 0) {
 		return 'undefined';
 	} else {
@@ -16,7 +18,8 @@ function getPrimes(n) {
 
 
 function isPrime(n) {
-
+	// takes as input, n an int and returns true if n is prime
+	// or false, otherwise
 	for (let i = 2; i < Math.floor(Math.sqrt(n) + 1); i++) {
 		if (n % i === 0)
 			return false;
