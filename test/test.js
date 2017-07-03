1cd86ab0cc3144288630a804fc4bc8b60602949c
Micah Akpan <mickey4sure2k7@gmail.com>
2017-07-03 15:27:15 +0000

    made adjustments to the assert function in test.js

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
-const assert = require('chai').expect;
+const assert = require('chai').assert;
 const mainApp = require('../src/main.js')
 
 describe("getPrimes", function() {
 
 	describe("handle valid input of n", function () {
 	it("should return primes numbers from 0 to n", function() {
-		assert(mainApp.getPrimes(10)).to.eql([2, 3, 5, 7]);
+		assert.deepEqual(mainApp.getPrimes(10), [2, 3, 5, 7]);
 	});
  })
 
