bc7d25c3b8bdb0e6d2c0f191f271780fedc26c4d
Micah Akpan <mickey4sure2k7@gmail.com>
2018-02-21 17:19:39 +0100

    Fix bug

--- eloquent_js/exercises/ch4/program.js
+++ eloquent_js/exercises/ch4/program.js
@@ -3,7 +3,7 @@ exports.range = (start, end, step) => {
   step = step || 1;
 
   if (step < 0) { // negative step
-    step *= step; // convert step  to a positive form
+    step *= -1; // convert step  to a positive form
     for (let i = start; i >= end; i -= step) {
       nums.push(i);
     }
