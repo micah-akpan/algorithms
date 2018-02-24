47b74b1a3ba0ffe88f5b1de3ea683ff312c3cdea
Micah Akpan <mickey4sure2k7@gmail.com>
2018-02-24 05:08:55 +0100

    Add 'some' function

--- eloquent_js/exercises/ch5/program.js
+++ eloquent_js/exercises/ch5/program.js
@@ -1,1 +1,7 @@
 exports.flatten = array => array.reduce((prev, curr) => prev.concat(curr));
+
+exports.some = (array, test) => {
+  const newArr = array.map(test);
+  const anyTrue = newArr.filter(val => val === true);
+  return anyTrue.length > 0;
+};
