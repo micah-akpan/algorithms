07de84fb53ac8c22d35533a0949e3595bf509d96
Micah Akpan <mickey4sure2k7@gmail.com>
2018-02-24 05:25:20 +0100

    Add different implementation of 'some array' function

--- eloquent_js/exercises/ch5/program.js
+++ eloquent_js/exercises/ch5/program.js
@@ -4,3 +4,12 @@ exports.some = (array, test) => {
   const anyTrue = array.map(test).filter(val => val === true);
   return anyTrue.length > 0;
 };
+
+exports.someV2 = (array, test) => {
+  array.forEach((value) => {
+    if (test(value)) {
+      return true;
+    }
+  });
+  return false;
+};
